bfda920e03e716ca837ce37acd5d4ce4db53fe16
Sam562retro <66123996+Sam562retro@users.noreply.github.com>
2020-07-08 14:12:23 +0530

    Create leapYear.cpp
    checks if a year is leap or not

---
+++ leapYear.cpp
@@ -0,0 +1,14 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a;
+cout<<"enter the year ""\n";
+cin>>a;
+if (a%4==0)
+cout<<" leap year ";
+else
+cout<<"normal year ";
+getch();
+}
