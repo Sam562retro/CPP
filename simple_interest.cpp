f42377365ab9daf89f5fe531f986d2c6570d11f7
Sam562retro <66123996+Sam562retro@users.noreply.github.com>
2020-07-08 14:16:32 +0530

    Create simple_interest.cpp
    gives out simple interest

---
+++ simple_interest.cpp
@@ -0,0 +1,10 @@
+#include<iostream.h>
+#include<conio.h>
+void main(){
+clrscr();
+int p,r,t,a;
+cout<<"enter three values ";
+cin>>p>>r>>t;
+a=(p*r*t)/100;
+cout<<"simple interest is "<<a;
+getch(); }
