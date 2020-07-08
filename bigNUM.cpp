d9a876f94ec21e206e905ab61dfa74fe2b906e87
Sam562retro <66123996+Sam562retro@users.noreply.github.com>
2020-07-08 14:04:21 +0530

    Create bigNUM.cpp
    compares two user defined numbers and output's the bigger one

---
+++ bigNUM.cpp
@@ -0,0 +1,16 @@
+#include <iostream.h>;
+main()
+{
+cout<<"please enter two numbers\n";
+int a,b;
+cin>>a;
+cin>>b;
+if
+(a>b)
+cout<<a<<"is bigger";
+else
+cout<<b<<"is bigger";
+int c;
+cin>>c;
+return 0;
+}
