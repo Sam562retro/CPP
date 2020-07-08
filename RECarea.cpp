e9beaa707118fcc47c53b95c6a16e689693e3e06
Sam562retro <66123996+Sam562retro@users.noreply.github.com>
2020-07-08 13:59:48 +0530

    Create RECarea.cpp
    takes out the area of a rectangle with length and breadth user defined

---
+++ RECarea.cpp
@@ -0,0 +1,12 @@
+#include<iostream.h>
+#include<conio.h>
+void main()
+{
+clrscr();
+int a,b,c;
+cout<<"enter the length and breath of rectangle ";
+cin>>a>>b;
+c=(a*b);
+cout <<" area of rectangle with length "<< a <<" and breadth "<<b<<" is "<<c;
+getch();
+}
