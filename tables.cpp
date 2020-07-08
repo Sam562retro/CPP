8c1b41b9f156fa93d7db02bf8483042c3e60b13e
Sam562retro <66123996+Sam562retro@users.noreply.github.com>
2020-07-08 14:18:46 +0530

    Create tables.cpp
    not the most efficient, (should have used for loop), but this is the table giver program that i made as my 5th program.

---
+++ tables.cpp
@@ -0,0 +1,19 @@
+#include<iostream.h>
+#include<conio.h>
+void main(){
+clrscr();
+int a;
+cout<<"table of ";
+cin>>a;
+cout<<a<<" * 1 = "<<(a*1)<<"  ""\n";
+cout<<a<<" * 2 = "<<(a*2)<<"  ""\n";
+cout<<a<<" * 3 = "<<(a*3)<<"  ""\n";
+cout<<a<<" * 4 = "<<(a*4)<<"  ""\n";
+cout<<a<<" * 5 = "<<(a*5)<<"  ""\n";
+cout<<a<<" * 6 = "<<(a*6)<<"  ""\n";
+cout<<a<<" * 7 = "<<(a*7)<<"  ""\n";
+cout<<a<<" * 8 = "<<(a*8)<<"  ""\n";
+cout<<a<<" * 9 = "<<(a*9)<<"  ""\n";
+cout<<a<<" * 10 = "<<(a*10)<<"  ""\n";
+getch();
+}
